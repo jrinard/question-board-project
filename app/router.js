fd5665f55c6e7f474423d5d3b270dc6655f05e86
joshua <jrinard@hotmail.com>
2017-01-12 19:15:34 -0800

    working display of post detail

--- app/router.js
+++ app/router.js
@@ -7,7 +7,8 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('post'), {path: '/post/:post_id'};
+  this.route('post', {path: '/post/:post_id'});
+
 });
 
 export default Router;
