811b729104b63bab6bd2f39ccd8000bf50e1172e
joshua <jrinard@hotmail.com>
2017-01-12 19:14:20 -0800

    add post-detail route

--- app/routes/post.js
+++ app/routes/post.js
@@ -1,9 +1,41 @@
-import DS from 'ember-data';
+import Ember from 'ember';
 
-export default DS.Model.extend({
-  question: DS.attr(),
-  notes: DS.attr(),
-  author: DS.attr(),
-  image: DS.attr()
-  // answers: DS.hasMany('answer', { async: true })
+export default Ember.Route.extend({
+  model(params){
+    return this.store.findRecord('post', params.post_id);
+  }
+  // ,
+  // actions:{
+  //   update(post, params){
+  //     Object.keys(params).forEach(function (key){
+  //       if (params[key] !== undefined){
+  //         post.set(key, params[key]);
+  //       }
+  //     });
+  //     post.save();
+  //     this.transitionTo('index');
+  //   },
+  //   destroyPost(post){
+  //     var comment_deletions = post.get('comments').map(function(comment){
+  //       return comment.destroyRecord();
+  //     });
+  //     Ember.RSVP.all(comment_deletions).then(function(){
+  //       return post.destroyRecord();
+  //     });
+  //     this.transitionTo('index');
+  //   },
+  //   saveComment(params) {
+  //     var newComment = this.store.createRecord('comment', params);
+  //     var post = params.post;
+  //     post.get('comments').addObject(newComment);
+  //     newComment.save().then(function(){
+  //       return post.save();
+  //     });
+  //     this.transitionTo('post', post);
+  //   },
+  //   destroyComment(comment) {
+  //     comment.destroyRecord(); /// needs to stay record
+  //     this.transitionTo('index');
+  //   }
+  // }
 });
