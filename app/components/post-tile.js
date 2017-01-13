8da896a6b6af5daa78dd8a40c207fec06a7a86eb
joshua <jrinard@hotmail.com>
2017-01-12 18:23:13 -0800

    Add post-tile and post route

---
+++ app/components/post-tile.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  isImageShowing: false,
+  actions: {
+    imageShow: function() {
+      this.set('isImageShowing', true);
+    },
+    imageHide: function() {
+      this.set('isImageShowing', false);
+    },
+  }
+});
